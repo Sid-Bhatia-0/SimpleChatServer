c6ce19ad78a81ab8572057f3fcbc9f3400cfb26b
Siddharth Bhatia <sidb60200@gmail.com>
2021-11-14 00:50:18 +0530

    add multi-client message broadcasting

--- server.jl
+++ server.jl
@@ -1,25 +1,48 @@
 import Sockets
 
 function start(port)
+    sockets = Sockets.TCPSocket[]
     server = Sockets.listen(port)
 
-    while true
-        println("listening...")
+    task = @async while true
         socket = Sockets.accept(server)
-        println("accepted a connection")
 
-        while true
+        i = findfirst(!isopen, sockets)
+
+        if isnothing(i)
+            push!(sockets, socket)
+            client_id = length(sockets)
+        else
+            sockets[i] = socket
+            client_id = i
+        end
+
+        println("New client: $(client_id)")
+
+        @async while isopen(socket)
             try
                 line = readline(socket)
-                println(socket, "You said: " * line)
-                println(stdout, "Client said: " * line)
+                if line != ""
+                    println("Client $(client_id) wants to broadcast $(line)")
+                    for socket_receiver in sockets
+                        if isopen(socket_receiver)
+                            try
+                                println(socket_receiver, line)
+                            catch error
+                                println(error)
+                                close(socket_receiver)
+                            end
+                        end
+                    end
+                end
             catch error
                 println(error)
-                close(socket)
-                break
+                close(socket_sender)
             end
         end
     end
+
+    wait(task)
 
     return nothing
 end
