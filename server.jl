1cfe830bba8f2c3c7e844e4a3e3212568aa195d8
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2023-01-20 14:52:51 -0600

    refactor (#19)
    * add function is_valid_nickname
    
    * add function is_valid_message
    
    * add function handle_socket
    
    * rename SERVER_HOST to SERVER_IP_ADDRESS for clarity

--- server.jl
+++ server.jl
@@ -1,6 +1,6 @@
 import Sockets
 
-const SERVER_HOST = Sockets.localhost # Sockets.ip"127.0.0.1"
+const SERVER_IP_ADDRESS = Sockets.localhost # Sockets.ip"127.0.0.1"
 const SERVER_PORT = 50000
 
 function try_send(socket, message)
@@ -24,6 +24,54 @@ function try_broadcast(room, message)
     return nothing
 end
 
+is_valid_nickname(nickname) = occursin(r"^[A-Za-z0-9]{1,32}$", nickname)
+
+is_valid_message(message) = all(char -> isprint(char) && isascii(char), message)
+
+function handle_socket(room, room_lock, socket)
+    try_send(socket, "Enter a nickname")
+    nickname = readline(socket)
+
+    if is_valid_nickname(nickname)
+        user_entry_message = "[$(nickname) has entered the room]"
+        lock(room_lock) do
+            push!(room, socket)
+            try_broadcast(room, user_entry_message)
+        end
+
+        while !eof(socket)
+            user_message = readline(socket)
+            if is_valid_message(user_message)
+                broadcast_message = "$(nickname): $(user_message)"
+                lock(room_lock) do
+                    try_broadcast(room, broadcast_message)
+                end
+            else
+                @info "(peername = $(peername)) invalid message"
+                try_send(socket, "[ERROR: message must be composed only of printable ascii characters]")
+                close(socket)
+                break
+            end
+        end
+
+        close(socket)
+
+        user_exit_message = "[$(nickname) has left the room]"
+        lock(room_lock) do
+            pop!(room, socket)
+            try_broadcast(room, user_exit_message)
+        end
+    else
+        @info "(peername = $(peername)) invalid nickname"
+        try_send(socket, "[ERROR: nickname must be composed only of a-z, A-Z, and 0-9 and its length must be between 1 to 32 characters (both inclusive)]")
+        close(socket)
+    end
+
+    @info "(peername = $(peername)) socket disconnected"
+
+    return nothing
+end
+
 function start_server(server_host, server_port)
     room = Set{Sockets.TCPSocket}()
 
@@ -38,50 +86,10 @@ function start_server(server_host, server_port)
         peername = Sockets.getpeername(socket)
         @info "(peername = $(peername)) socket accepted"
 
-        @async begin
-            try_send(socket, "Enter a nickname")
-            nickname = readline(socket)
-
-            if occursin(r"^[A-Za-z0-9]{1,32}$", nickname)
-                user_entry_message = "[$(nickname) has entered the room]"
-                lock(room_lock) do
-                    push!(room, socket)
-                    try_broadcast(room, user_entry_message)
-                end
-
-                while !eof(socket)
-                    user_message = readline(socket)
-                    if all(char -> isprint(char) && isascii(char), user_message)
-                        broadcast_message = "$(nickname): $(user_message)"
-                        lock(room_lock) do
-                            try_broadcast(room, broadcast_message)
-                        end
-                    else
-                        @info "(peername = $(peername)) invalid message"
-                        try_send(socket, "[ERROR: message must be composed only of printable ascii characters]")
-                        close(socket)
-                        break
-                    end
-                end
-
-                close(socket)
-
-                user_exit_message = "[$(nickname) has left the room]"
-                lock(room_lock) do
-                    pop!(room, socket)
-                    try_broadcast(room, user_exit_message)
-                end
-            else
-                @info "(peername = $(peername)) invalid nickname"
-                try_send(socket, "[ERROR: nickname must be composed only of a-z, A-Z, and 0-9 and its length must be between 1 to 32 characters (both inclusive)]")
-                close(socket)
-            end
-
-            @info "(peername = $(peername)) socket disconnected"
-        end
+        @async handle_socket(room, room_lock, socket)
     end
 
     return nothing
 end
 
-start_server(SERVER_HOST, SERVER_PORT)
+start_server(SERVER_IP_ADDRESS, SERVER_PORT)
